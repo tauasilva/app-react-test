3fee4cbb9a8c997cccd48cff78342c394d0735d1
Tauã Fagundes <tauafagundessilva@gmail.com>
2025-05-28 09:38:09 -0300

    teste endpoint

--- server/app.py
+++ server/app.py
@@ -27,8 +27,17 @@ app = FastAPI()
 ui_app = StaticFiles(directory="client/dist", html=True)
 api_app = FastAPI()
 
+
+# Endpoint simples adicionado à sub-aplicação de API
+@api_app.get("/hello")
+def read_hello():
+    return {"message": "Hello, World!"}
+
+
+
 # PLEASE NOTE THE ORDER OF THE MOUNTS MATTERS
 app.mount("/", ui_app)
+app.mount("/api", api_app)
 
 origins = [
     "http://localhost:3000",
